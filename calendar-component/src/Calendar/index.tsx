ac268f317eafccc94e2221ffb8fed87937dc2b86
Si3ver <izhouwl@163.com>
2024-03-01 15:37:40 +0800

    feat: init calendar comp

---
+++ calendar-component/src/Calendar/index.tsx
@@ -0,0 +1,9 @@
+import "./index.scss";
+
+function Calendar() {
+  return <div className="calendar">
+    111
+  </div>
+}
+
+export default Calendar;
---
+++ calendar-component/src/test.js
@@ -0,0 +1,8 @@
+const dayjs = require('dayjs');
+
+console.log(dayjs('2024-3-1').daysInMonth());
+
+console.log(dayjs('2024-3-1').startOf('month').format('YYYY-MM-DD'));
+
+console.log(dayjs('2024-3-1').endOf('month').format('YYYY-MM-DD'));
+
