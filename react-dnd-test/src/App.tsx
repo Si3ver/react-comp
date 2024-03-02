382f2ee69ccf12aed987af4f80e3cfc1240b1908
Si3ver <izhouwl@163.com>
2024-03-02 22:36:24 +0800

    feat: init two ele

--- react-dnd-test/src/App.tsx
+++ react-dnd-test/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function Box() {
+  return <div className='box'></div>
+}
+
+function Container() {
+  return <div className="container"></div>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <Container></Container>
+    <Box></Box>
+  </div>
 }
 
 export default App;
