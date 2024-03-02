9e35474731ba906a606bcb527a3b075ceaea9197
Si3ver <izhouwl@163.com>
2024-03-02 23:26:43 +0800

    feat: init Icon.tsx

---
+++ icon-component/src/Icon/index.tsx
@@ -0,0 +1,23 @@
+import React, { PropsWithChildren, forwardRef } from "react";
+
+type BaseIconProps = {
+  className?: string;
+  style?: React.CSSProperties;
+  size?: string | string[];
+  spin?: boolean;
+};
+
+export type IconProps = BaseIconProps &
+  Omit<React.SVGAttributes<SVGElement>, keyof BaseIconProps>;
+
+export const Icon = forwardRef<SVGSVGElement, PropsWithChildren<IconProps>>(
+  (props, ref) => {
+    const { style, className, spin, size = "1em", children, ...rest } = props;
+
+    return (
+      <svg ref={ref} style={style} fill="currentColor" {...rest}>
+        {children}
+      </svg>
+    );
+  }
+);
