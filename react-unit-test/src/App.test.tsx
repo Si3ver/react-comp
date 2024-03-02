a75662a096a94d2c78b51e006562ee9b71cc47b1
Si3ver <izhouwl@163.com>
2024-03-02 15:14:22 +0800

    feat: unit test for comp toggle.tsx

--- react-unit-test/src/App.test.tsx
+++ react-unit-test/src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import Toggle from './Toggle';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2', () => {
+  const { container } = render(<App />);
+  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+  const linkElement = container.querySelector('.App-link');
+
+  expect(linkElement?.textContent).toMatch(/learn react/i)
+});
+
+test('toggle', () => {
+  const {container} = render(<Toggle />);
+
+  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+  expect(container.querySelector('p')?.textContent).toBe('close');
+
+  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+  fireEvent.click(container.querySelector('button')!);
+
+  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+  expect(container.querySelector('p')?.textContent).toBe('open');
 });
