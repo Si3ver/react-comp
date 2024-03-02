257a8e42c86bfa1418a4b36e2f0ce988eb3fc1aa
Si3ver <izhouwl@163.com>
2024-03-02 22:46:29 +0800

    feat: first try of drag and drop

--- react-dnd-test/src/App.tsx
+++ react-dnd-test/src/App.tsx
@@ -1,11 +1,36 @@
+import { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 import './App.css';
 
 function Box() {
-  return <div className='box'></div>
+  const ref = useRef(null);
+
+  const [, drag]= useDrag({
+    type: 'box',
+    item: {
+      color: 'blue'
+    }
+  });
+
+  drag(ref);
+
+  return <div ref={ref} className='box'></div>
 }
 
 function Container() {
-  return <div className="container"></div>
+  const ref = useRef(null);
+
+  const [,drop] = useDrop(() => {
+    return {
+      accept: 'box',
+      drop(item) {
+        console.log(item);
+      }
+    }
+  });
+  drop(ref);
+
+  return <div ref={ref} className="container"></div>
 }
 
 function App() {
