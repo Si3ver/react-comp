aeafb5f817280560344b1bafb2199f8a8b21c10c
Si3ver <izhouwl@163.com>
2024-03-03 18:22:40 +0800

    feat: use zustand middleware & subscribe

--- zustand-test/src/App.jsx
+++ zustand-test/src/App.jsx
@@ -1,15 +1,36 @@
+import { useEffect } from "react";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useXxxStore = create((set) => ({
+// 自定义 zustand 中间件
+// function logMiddleware(func) {
+//   return function(set, get, store) {
+//     function newSet(...args) {
+//       console.log('调用了 set，新的 state: ', get());
+//       return set(...args);
+//     }
+//     return func(newSet, get, store);
+//   }
+// }
+
+const useXxxStore = create(persist((set) => ({
   aaa: "",
   bbb: "",
   updateAaa: (value) => set(() => ({ aaa: value })),
   updateBbb: (value) => set(() => ({ bbb: value })),
+}), {
+  name: 'XxxStore',
 }));
 
 export default function App() {
   const updateAaa = useXxxStore((state) => state.updateAaa);
   const aaa = useXxxStore((state) => state.aaa);
+
+  useEffect(() => {
+    useXxxStore.subscribe((state) => {
+      console.log(useXxxStore.getState());
+    });
+  }, []);
 
   return (
     <div>
