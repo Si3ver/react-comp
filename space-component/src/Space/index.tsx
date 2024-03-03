784f1b66e643b600155a530c6593b6b9aa9ce1e6
Si3ver <izhouwl@163.com>
2024-03-03 21:53:57 +0800

    feat: layout

--- space-component/src/Space/index.tsx
+++ space-component/src/Space/index.tsx
@@ -1,4 +1,6 @@
+import classNames from "classnames";
 import React from "react";
+import './index.scss';
 
 export type SizeType = 'small' | 'middle' | 'large' | number | undefined;
 
@@ -16,14 +18,23 @@ const Space: React.FC<SpaceProps> = props => {
   const {
     className,
     style,
-    size, 
-    direction,
+    size = 'small', 
+    direction = 'horizontal',
     align,
     split,
     ...otherProps
   } = props;
 
   const childNodes = React.Children.toArray(props.children);
+  const mergedAlign = direction === 'horizontal' && align === undefined ? 'center' : align;
+  const cn = classNames(
+    'space',
+    `space-${direction}`,
+    {
+      [`space-align-${mergedAlign}`]: mergedAlign,
+    },
+    className,
+  );
   const nodes = childNodes.map((child: any, i) => {
     const key = child && child.key || `space-item-${i}`;
 
@@ -33,7 +44,7 @@ const Space: React.FC<SpaceProps> = props => {
   });
 
   return <div
-    className={className}
+    className={cn}
     style={style}
     {...otherProps}
   >
