614f2457f9d51845498bef7aa62484381082451b
Si3ver <izhouwl@163.com>
2024-03-29 00:43:24 +0800

    fix: test useEffect deps arr

--- mini-react/src/index.jsx
+++ mini-react/src/index.jsx
@@ -1,17 +1,21 @@
 const { render, useState, useEffect } = window.MiniReact;
 
 function App() {
-  const [count, setCount] = useState(1);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(100);
 
   useEffect(() => {
-    console.log('useEffect', count);
-  }, []);
+    console.log('useEffect log:', count1, count2);
+  }, [count1]);
 
   return (
     <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(prev => prev + 1)}>+</button>
-      <button onClick={() => setCount(prev => prev - 1)}>-</button>
+      <p>{count1}</p>
+      <button onClick={() => setCount1(prev => prev + 1)}>plus count1</button>
+      <button onClick={() => setCount1(prev => prev - 1)}>minus count1</button>
+      <p>{count2}</p>
+      <button onClick={() => setCount2(prev => prev + 1)}>plus count2</button>
+      <button onClick={() => setCount2(prev => prev - 1)}>minus count2</button>
       <h1>h1</h1>
       <p>
         <h2>h2</h2>
