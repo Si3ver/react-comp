4374e65e5d510dfb01851c03cf2d1977f8926a53
Si3ver <izhouwl@163.com>
2024-03-29 00:40:00 +0800

    fix: test useState & useEffect

--- mini-react/src/index.jsx
+++ mini-react/src/index.jsx
@@ -1,9 +1,17 @@
 const { render, useState, useEffect } = window.MiniReact;
 
 function App() {
-  
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    console.log('useEffect', count);
+  }, []);
+
   return (
     <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(prev => prev + 1)}>+</button>
+      <button onClick={() => setCount(prev => prev - 1)}>-</button>
       <h1>h1</h1>
       <p>
         <h2>h2</h2>
